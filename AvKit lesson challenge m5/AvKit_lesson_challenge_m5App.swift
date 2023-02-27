bbb146323c3d61cc2725ef325aaa4015a37995c9
Stephen Brady <bradystephen71@yahoo.co.uk>
2023-02-27 17:29:04 +0000

    Initial Commit

---
+++ AvKit lesson challenge m5/AvKit_lesson_challenge_m5App.swift
@@ -0,0 +1,17 @@
+//
+//  AvKit_lesson_challenge_m5App.swift
+//  AvKit lesson challenge m5
+//
+//  Created by Stephen Brady on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvKit_lesson_challenge_m5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
